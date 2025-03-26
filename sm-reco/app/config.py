a4cd41023e6b5bfa274976be72e5287021b9ca77
daintree-henry <hyeonwoohwang@hyeonwooui-Macmini.local>
2025-03-26 22:32:20 +0900

    20250326

--- sm-reco/app/config.py
+++ sm-reco/app/config.py
@@ -2,13 +2,8 @@ import os
 
 
 class Config:
-    # Construct database URI from individual components
     SUB_URL = os.getenv('SUB_URL', 'http://sm-subs')
 
-    # Return Recommend Count
-    RECOMMEND_COUNT = os.getenv('RECOMMEND_COUNT', '1')
-
-    # JWT Configuration
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'mJMLk2qwEFKp1Lx2FatzwVOA6-3FjMqkLEAWu74uCU9')
 
     REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
@@ -17,4 +12,7 @@ class Config:
     REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', 'redispassword')
 
     CACHE_KEY = os.getenv('CACHE_KEY', 'SUB_PLANS')
-    CACHE_TTL_SECOND = int(os.getenv('CACHE_TTL_SECOND', '60'))
+    SUBS_CACHE_TTL_SECOND = int(os.getenv('SUBS_CACHE_TTL_SECOND', '60'))
+
+    RECOMMEND_COUNT = os.getenv('RECOMMEND_COUNT', '1')
+    RECOMMEND_CACHE_TTL_SECOND = int(os.getenv('RECOMMEND_CACHE_TTL_SECOND', '60'))
